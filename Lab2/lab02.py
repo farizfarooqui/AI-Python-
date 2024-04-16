6bdfadecba331109b4e5e6b09aa06719e0913ae8
MUHAMMAD FARIZ FAROOQUI <135133746+farizfarooqui@users.noreply.github.com>
2024-04-16 22:29:07 +0500

    lab2

--- Lab2/lab02.py
+++ Lab2/lab02.py
@@ -1,15 +1,13 @@
 import numpy as np
 print('********************* Lab 02 *****************************')
-print('********************* In Lab Task 01 *****************************')
-print("")
+print('********************* In Lab Task *****************************')
 num = np.ones((4, 4), dtype=int)
 #set the inner elements to 0
 num[1:-1, 1:-1] = 0
 print(num)
 
 print("\n")
-print('********************* In Post Task 01 *****************************')
-print("")
+print('********************* Post Lab Task *****************************')
 #creating 8by8 matrix filled with zeros
 matrix = np.zeros((8, 8), dtype=int)
 #filling the matrix with the checkerboard pattern
