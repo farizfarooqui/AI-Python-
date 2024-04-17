b52ad8ef8b1bf7f4500a0ab20e30849222a8fc9e
MUHAMMAD FARIZ FAROOQUI <135133746+farizfarooqui@users.noreply.github.com>
2024-04-18 00:13:21 +0500

    done

--- Lab1/Lab01.py
+++ Lab1/Lab01.py
@@ -1,24 +1,19 @@
-print('********************* In Lab Task 01 *****************************')
+#In lab task 1
 daily_expenses = [80, 100, 90, 120, 110, 130, 70, 150, 140, 160, 200, 180, 190, 210, 220, 250, 240, 230, 270, 260, 280, 300, 320, 310, 340, 330, 350, 370, 360, 380]
-# i am assuming weekends are the top 8 spending days
 weekend_expenses = daily_expenses[-8:]
 print("Weekend spendings :",weekend_expenses)
 total_weekend_expenses = sum(weekend_expenses)
-print("Total expenses for the weekends:", total_weekend_expenses)
-print("")
+print("Total expenses", total_weekend_expenses)
 
 
-print('********************* In Lab Task 02 *****************************')
+#In lab task 2
 monthly_sales = (1200, 1500, 1400, 1600, 1800, 1900, 2000, 2200, 2100, 2300, 2400, 2500, 2600, 2700, 2800, 2900, 3000, 3100, 3200, 3300, 3400, 3500, 3600, 3700, 3800, 3900, 4000, 4100, 4200, 4300)
-#second half of the month using negative indexing
 second_half_sales = monthly_sales[-15:]
 total_sales_second_half = sum(second_half_sales)
 print("Total sales for the second half of the month:", total_sales_second_half)
-print("")
 
 
-print('********************* Post lab task *****************************')
-#second half of the month using negative indexing
+#post lab task
 sat_sales = monthly_sales[-2::-7]
 sun_sales = monthly_sales[-1::-7]
 total_sales_second_half = sum(sat_sales+sun_sales)
--- Lab2/lab02.py
+++ Lab2/lab02.py
@@ -1,16 +1,11 @@
 import numpy as np
-print('********************* Lab 02 *****************************')
-print('********************* In Lab Task *****************************')
-num = np.ones((4, 4), dtype=int)
-#set the inner elements to 0
+#In lab task
+num = np.ones((8, 8), dtype=int)
 num[1:-1, 1:-1] = 0
 print(num)
 
-print("\n")
-print('********************* Post Lab Task *****************************')
-#creating 8by8 matrix filled with zeros
+#post lab task
 matrix = np.zeros((8, 8), dtype=int)
-#filling the matrix with the checkerboard pattern
 matrix[::2, ::2] = 0 
 matrix[1::2, 1::2] = 0
 matrix[::2, 1::2] = 1 
